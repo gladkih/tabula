8bd864f415ea0ba792ec3e40087cd9498ac5879a
Maxim Gladkih <mail@gladkih.su>
2016-03-01 13:44:47 +0300

    Вывожу список файлов, которые скопированы.

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,6 +1,7 @@
 import config from '../config';
 const gulp = require('gulp');
 const newer = require('gulp-newer');
+const debug = require('gulp-debug');
 
 gulp.task('assets', done => {
   if (!config.assets.length) {
@@ -9,5 +10,6 @@ gulp.task('assets', done => {
   }
   return gulp.src(config.assets, {since: gulp.lastRun('assets')})
     .pipe(newer(config.assetsDest))
+    .pipe(debug({title: 'assets'}))
     .pipe(gulp.dest(config.assetsDest));
 });
