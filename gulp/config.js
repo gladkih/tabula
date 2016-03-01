714edc3b58aaa4326007287f53e46575fc2b6a49
Maxim Gladkih <mail@gladkih.su>
2016-03-01 13:38:15 +0300

    Исправлена маска для ассетов.

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,7 @@
 export default {
   dist: 'build',
   templates: ['app/templates/**/*.jade', '!app/templates/**/_*'],
-  assets: ['app/assets/'],
+  assets: ['app/assets/**/*.*'],
   assetsDest: 'build/static/assets/',
   page: 'app/templates/**/*.jade',
   particles: 'app/templates/**/_*.jade',
--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -1,11 +1,13 @@
 import config from '../config';
 const gulp = require('gulp');
+const newer = require('gulp-newer');
 
 gulp.task('assets', done => {
   if (!config.assets.length) {
     done();
     return null;
   }
-  return gulp.src(`${config.assets}**/*.*`, {since: gulp.lastRun('assets')})
+  return gulp.src(config.assets, {since: gulp.lastRun('assets')})
+    .pipe(newer(config.assetsDest))
     .pipe(gulp.dest(config.assetsDest));
 });
